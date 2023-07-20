44350643a5c9ab962ff8fc9f2555b6e074c7760f
tbello <bello4aus@gmail.com>
2023-07-20 14:37:58 +0100

    - add test for rendering page when user is present in the localstorage

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,15 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, getByTestId } from '@testing-library/react';
 import { useRouter } from 'next/router';
 import Home from '@/pages/index';
 
+const setLocalStorage = (key: string, data: string) => {
+  window.localStorage.setItem(key, JSON.stringify(data));
+};
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn()
 }))
 
 
-describe('Home', () => {
-    it('renders a logout', () => {
+describe('Test for HomePage', () => {
+    it('renders an empty page when user is absent in localstorage', () => {
       
       const pushMock = jest.fn()
       useRouter.mockReturnValue({
@@ -18,4 +21,15 @@ describe('Home', () => {
         const {container} = render(<Home />)
         expect(container).toBeEmptyDOMElement()       
     })
+
+    it('render page when user is present in localstorage', () => {
+        setLocalStorage('user', '{username: tomi}')
+        render(<Home />)
+        
+      const heading = screen.getByRole('heading', {
+        name: /You are logged In/i,
+      })
+
+      expect(heading).toBeInTheDocument()
+      })
 })
