47796b32d1c342db7b82b63f776577373ec8428c
tbello <bello4aus@gmail.com>
2023-07-21 12:47:18 +0100

    add layout component in index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,59 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-export default function Home() {
+import { Button } from 'antd';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const getItem = (
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+): MenuItem => {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+
+
+const items: MenuItem[] = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
+
+
+const  Home = () => {
   const router = useRouter()
   const [user, setUser] = useState<{username: string} | null>(null)
+  const [collapsed, setCollapsed] = useState(true)
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   const handleLogout = () => {
     localStorage.removeItem('user')
     router.push('/login')
@@ -19,29 +70,30 @@ export default function Home() {
   }, [router])
   if (user) {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Welcome&nbsp;
-            <code className="font-mono font-bold">{user.username}</code>
-          </p>
-        </div>
-        <h1 className={`mb-3 text-2xl font-semibold`}> You are logged In </h1>
-        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Click the button below to logout
-          </p>
-          <button
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            onClick={handleLogout}>
-            Logout{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </button>  
-        </div>
+      <main className="bg-dark">
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+            <div className="demo-logo-vertical"></div>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+          </Sider>
+          <Layout style={{ padding: '0 16px', background: '#000' }}>
+            <div className="">
+              <p className="">
+                Welcome&nbsp;
+                <code className="font-mono font-bold">{user.username}</code>
+              </p>
+            </div>
+            <Content >
+            <div>
+              <Button type="primary"  onClick={handleLogout}> Logout </Button>
+            </div>
+            </Content>
+          </Layout>
+        </Layout>
       </main>
     )  
   }
   return;
  }
+
+ export default Home;
