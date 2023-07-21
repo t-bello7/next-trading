b4525f802654b5913f3b88356923447d285f9197
tbello <bello4aus@gmail.com>
2023-07-21 13:06:05 +0100

    - implement custom theme with antd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import './globals.css'
+import './globals.css';
+import  { ConfigProvider } from 'antd';
 import { AppProps } from 'next/app';
+import theme from '@/utils/themeConfig';
 
 interface CustomPageProps { // <--- your custom page props
    // your props
 }
 
-export default function App({ Component, pageProps }: AppProps<CustomPageProps>) {
-    return<Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps<CustomPageProps>) => (
+    <ConfigProvider theme={theme}>
+        <Component {...pageProps} />
+    </ConfigProvider>
+)
+
+export default App;
   
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,26 +1,78 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
+import Document, {  DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
 
-export default class _Document extends Document {
+const MyDocument = () => (
+  <Html lang="en">
+    <Head>
+      <title>AssetxPro</title>
+      <link
+      rel="shortcut icon"
+      href="vercel.svg"
+      // type="image/x-icon"
+      />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#fff" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* <title>AssetxPro</title> */}
-          {/* <link
-            rel="shortcut icon"
-            href="alarm-clock.png"
-            type="image/x-icon"
-          /> */}
-          <link rel="manifest" href="/manifest.json" />
-          {/* <meta name="theme-color" content="#fff" /> */}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) =>
+        (
+          <StyleProvider cache={cache}>
+            <App {...props} />
+          </StyleProvider>
+        ),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  // 1.1 extract style which had been used
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        {/* 1.2 inject css */}
+        <style dangerouslySetInnerHTML={{ __html: style }}></style>
+      </>
+    ),
+  };
+};
+
+export default MyDocument;
+
+
+// export default class _Document extends Document {
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           {/* <title>AssetxPro</title> */}
+//           {/* <link
+//             rel="shortcut icon"
+//             href="alarm-clock.png"
+//             type="image/x-icon"
+//           /> */}
+//           <link rel="manifest" href="/manifest.json" />
+//           {/* <meta name="theme-color" content="#fff" /> */}
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
